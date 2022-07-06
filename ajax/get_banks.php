86897a9e6d8ed4dcbb9a73b082caeb02e590b668
mukash <mkwasti@gmail.com>
2022-07-06 23:40:19 +0500

    partial for banks

---
+++ ajax/get_banks.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+require '../config/config.php';
+
+
+$kyc_info = KycVerification::getInfoByUserID($_SESSION['id']);
+
+include('../templates/partials/fetch_user_banks.phtml');
